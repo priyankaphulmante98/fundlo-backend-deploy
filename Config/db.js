27ba414d874ddbecdfb0629b3ad7219536e8789e
priyanka <priyankaphulmante98@gmail.com>
2023-03-10 15:23:58 +0530

    deploy backend

--- Config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-require("dotenv").config()
-
-mongoose.set({strictQuery: false})
-
-mongoose.connect("mongodb+srv://shreedhar:shreedhar@cluster0.urmb4ef.mongodb.net/fundflo?retryWrites=true&w=majority").then(() => 
-console.log('db.connected')).catch((err) => console.log(err))
